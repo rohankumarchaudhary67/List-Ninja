643cbe1a50cc70305d3cb5bbc98539393d18cf62
rohankumarchaudhary67 <rohankumarchaudhary67@gmail.com>
2023-07-10 02:12:12 +0530

    Adding theme context

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,16 @@
-export default function Auth(){
+"use client"
+import { Context } from "@/lib/context/Context"
+import { useContext } from "react"
+
+export default function Auth() {
+
+    const { themeValue, toggleTheme } = useContext(Context)
+
     return (
-        <p>sad</p>
+        <>
+            <div className={`flex justify-center items-center h-screen w-screen ${themeValue == 'light' ? 'bg-white' : 'bg-black'}`}>
+                <button onClick={toggleTheme} className={`${themeValue == 'light' ? 'text-white bg-black' : 'text-black bg-white'} border-4 px-4 py-2`}>Change Theme</button>
+            </div>
+        </>
     )
 }
