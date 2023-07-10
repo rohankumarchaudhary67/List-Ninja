13e2a50dd88debdc86567d2fcc14d6180b70083b
rohankumarchaudhary67 <rohankumarchaudhary67@gmail.com>
2023-07-11 00:18:15 +0530

    Adding ref in Button Componenet

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,4 +1,5 @@
-import { ButtonHTMLAttributes, FC } from "react";
+
+import { ButtonHTMLAttributes, forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/lib/utils/utils";
 
@@ -7,17 +8,17 @@ const buttonVariants = cva(
     'py-2 px-4 rounded-lg bg-blue text-white',
     {
         variants: {
-            variant:{
+            variant: {
                 default: 'bg-grey text-black',
                 dark: 'bg-black text-white'
             },
-            size:{
+            size: {
                 default: 'py-2 px-4',
                 lg: 'py-3 px-6'
             }
         },
-        defaultVariants:{
-            variant:'default',
+        defaultVariants: {
+            variant: 'default',
             size: 'default'
         }
     }
@@ -27,8 +28,13 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantPr
 
 }
 
-const Button: FC<ButtonProps> = ({ className, size, variant, ...props}) => {
-    return <button className={cn(buttonVariants({variant, size, className}))} {...props}/>
-}
+// eslint-disable-next-line react/display-name
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, size, variant, ...props }, ref) => {
+        return (
+            <button ref={ref} className={cn(buttonVariants({ variant, size, className }))} {...props} />
+        )
+    }
+)
 
-export {Button, buttonVariants};
+export { Button, buttonVariants };
