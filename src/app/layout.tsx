34739c0e5149ff9b9d8e59a8265d945c0fdc7ac6
rohankumarchaudhary67 <rohankumarchaudhary67@gmail.com>
2023-07-13 00:46:19 +0530

    Footer component added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children, }: { children: React.ReactElement
           <>
             <Navbar />
             {children}
+            <Footer />
           </>
         </ContextWrapper>
       </body>
