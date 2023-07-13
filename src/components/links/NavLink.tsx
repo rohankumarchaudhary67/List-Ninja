a1e813f392c7cc872fd455e6f98c5555a7314471
rohankumarchaudhary67 <rohankumarchaudhary67@gmail.com>
2023-07-13 12:19:58 +0530

    Link Component is added

---
+++ src/components/links/NavLink.tsx
@@ -0,0 +1,14 @@
+import { LinkProps } from "next/link";
+import { forwardRef } from "react";
+
+const NavLink = forwardRef<HTMLLinkElement, LinkProps>(
+    ({}, ref) => {
+        return(
+            <>
+            
+            </>
+        )
+    }
+)
+
+export {NavLink}
