b7612a8708a06979671aea414b5141338ad14201
rohankumarchaudhary67 <rohankumarchaudhary67@gmail.com>
2023-08-04 22:42:16 +0530

    Import added in Navlink.tsx file

---
+++ src/components/buttons/Navlink.tsx
@@ -0,0 +1,5 @@
+import { VariantProps } from "class-variance-authority";
+import { cn } from '@/lib/utils/utils'
+import { AnchorHTMLAttributes, forwardRef } from "react";
+import Link from "next/link";
+
